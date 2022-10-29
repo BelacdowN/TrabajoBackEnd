d28f5a17f3d67b25f61d350f5edd3504ab7012ac
BelacdowN <104211742+BelacdowN@users.noreply.github.com>
2022-10-29 09:34:28 -0300

    conectando a github

---
+++ src/main/java/com/miaplicacion/proyectohcc/ProyectohccApplication.java
@@ -0,0 +1,13 @@
+package com.miaplicacion.proyectohcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectohccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectohccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miaplicacion/proyectohcc/ProyectohccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaplicacion.proyectohcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectohccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
